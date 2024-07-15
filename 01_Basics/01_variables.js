b101e82156751be331b52aab6bdb9c0d3a2c5c9e
Anshul Vinchurkar <ansul241@gmail.com>
2024-07-15 11:36:55 +0000

    container changes done

--- 01_Basics/01_variables.js
+++ 01_Basics/01_variables.js
@@ -5,4 +5,9 @@ accountCity = "Indore"
 
 console.log(accountId);
 
+/*
+do not use var
+because of issue in block scope and functional scope
+*/
+
 console.table([accountId, accountEmail, accountPassword, accountCity]);
