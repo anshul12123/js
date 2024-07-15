7fe7f1d3bb535e3e4d1b2dce021ad79b73db3d8f
Anshul Vinchurkar <ansul241@gmail.com>
2024-07-15 11:35:34 +0000

    container changes done

---
+++ 01_Basics/01_variables.js
@@ -0,0 +1,8 @@
+const accountId = 5555
+let accountEmail = "test@gmail.com"
+var accountPassword = "123654"
+accountCity = "Indore"
+
+console.log(accountId);
+
+console.table([accountId, accountEmail, accountPassword, accountCity]);
