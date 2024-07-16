300a32e6fd48509046fad5aa1e001079172edd61
Anshul Vinchurkar <ansul241@gmail.com>
2024-07-16 10:56:34 +0000

    update string conversion

---
+++ 01_Basics/datatype_con.js
@@ -0,0 +1,30 @@
+let a = "sdd"
+
+// console.log(a);
+// console.log(typeof a);
+// console.log(typeof (a));
+
+// let valueInNumber = Number(a)
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
+
+// "33" => 33
+// "33abc" => NaN
+// true => 1 and false => 0
+
+let b = "abc"
+
+let boolerConvert = Boolean(b)
+// console.log(boolerConvert);
+
+//1 => true and 0=> false
+//"" => true
+//5 or any number => true
+//"name" => true
+
+
+let number = "true"
+
+let stringNumber = String(number)
+console.log(stringNumber);
+console.log(typeof stringNumber);
